5bbdc877543dac8fb547fe615d843061ca5715b8
antonchik <ganseanderson@gmail.com>
2022-01-14 15:35:52 +0300

    Добавьте файлы проекта.

---
+++ LessOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LessOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
