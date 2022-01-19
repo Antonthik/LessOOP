dd3ea5249805bac1b91b9f496ef6dcb98908fbe1
antonchik <ganseanderson@gmail.com>
2022-01-19 07:19:40 +0300

    Revert "Задание1. Перевод с счета на счет"
    This reverts commit b2e98032bf5b2eeb0be83be337fbe4e30a9bea18.

--- LessOOP/Program.cs
+++ LessOOP/Program.cs
@@ -16,8 +16,7 @@ namespace LessOOP
             Arr[2] = new Accounts(1, 400, (typeacc)1);
             Arr[3] = new Accounts(1, 70, (typeacc)1);
 
-            //Accounts acc = Arr[0];//текущий счет
-            Accounts acc;//текущий счет
+            Accounts acc = Arr[0];//текущий счет
 
             Console.WriteLine($"Доступные счета");
             foreach (Accounts accoun in Arr) 
@@ -28,21 +27,20 @@ namespace LessOOP
             Console.WriteLine($"Введите номер счета");
             var intextNum = Console.ReadLine();
             long  num = Convert.ToInt64(intextNum);
-            //boolean flag = false;
+            Boolean flag = false;
            
-            ////ищем выбранный счет
-            //foreach (accounts accnt in arr)
-            //{
-            //    if (num == accnt.numberaccounts) 
-            //    {
-            //        acc = accnt;
-            //        flag = true;
-            //    }  
-            //    if (flag == true) break;
-            //}
-            
-            //if (flag == true)
-            if (FindeAcc(num,Arr,out acc)==true)
+            //Ищем выбранный счет
+            foreach (Accounts accnt in Arr)
+            {
+                if (num == accnt.NumberAccounts) 
+                {
+                    acc = accnt;
+                    flag = true;
+                }  
+                if (flag == true) break;
+            }
+
+            if (flag == true)
             {
                
                 //Пополнение текущего счета
@@ -70,56 +68,15 @@ namespace LessOOP
                     Console.WriteLine($"Вы сняли сумму {sum2} и остаток на балансе { acc.BalansAccounts}");
                 }
 
-                // Списание со счета на счет
-                Console.WriteLine($"Введите номер счета для списания:");
-                var intextNum0 = Console.ReadLine();
-                long numdonor = Convert.ToInt64(intextNum0);
-                Accounts accdonor = Arr[0];//счет донор
-                if (FindeAcc(numdonor, Arr,out accdonor) == true)
-                {
-                    Console.WriteLine($"Введите сумму для снятия:");
-                    intext = Console.ReadLine();
-                    if (intext == "") intext = "0";
-                    decimal sumd = Convert.ToDecimal(intext);
-                    Console.WriteLine($"На балансе счета донора: {accdonor.BalansAccounts}");
-
-                   if (acc.FromAccToAcc(accdonor, sumd) == true)
-                    {
-                        Console.WriteLine($"На балансе счета донора (после): {accdonor.BalansAccounts}");
-                        Console.WriteLine($"На балансе текущуго счета(после): {acc.BalansAccounts}");
-                    }
-                    else 
-                    {
-                        Console.WriteLine($"Недостаточно средств для перевода со счета");
-                    }
-                }
-
-
-
             }
             else 
             {
                 Console.WriteLine($"Счет не найден.");
-            }  
-        }
-
-        static bool FindeAcc(long num,Accounts[] arr,out Accounts acc) 
-        {
-            bool flag=false;
-            acc = arr[0];//текущий счет
-            //Ищем выбранный счет
-            foreach (Accounts accnt in arr)
-            {
-                if (num == accnt.NumberAccounts)
-                {
-                    acc = accnt;
-                    flag = true;
-                }
-                if (flag == true) break;
             }
 
-            return flag;
+            
 
+  
         }
     }
 }
